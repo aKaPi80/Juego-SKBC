efc9abe7be43b37e890eda6e0aa3babb41db5b87
Alvaro Calvo <alvarocalvo8@gmail.com>
2026-06-01 22:13:56 +0200

    Update Kids async endpoint

--- kids/config.js
+++ kids/config.js
@@ -1,5 +1,5 @@
 window.SKBC_KIDS_CONFIG = {
   // Pega aqui la URL del Web App de Apps Script cuando lo publiques.
   // Debe terminar en /exec.
-  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbytjUnIwHBno82rvLoepufB-vPjxrAIgGO_4VDs9YJa4WbGNIpck1G6WenZbMx1p-q4vw/exec'
+  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycby91JjcrT_QHmBGyXGC7pB42npm9_Rx0RNr78k-OrwGp0Ci9jmG3b0X6XkJzKcaXCHjPg/exec'
 };
