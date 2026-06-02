9ece566f757cf2a31c2d8407885815646953b4d7
Alvaro Calvo <alvarocalvo8@gmail.com>
2026-06-02 13:56:47 +0200

    Point Kids portal to latest Web App

--- kids/config.js
+++ kids/config.js
@@ -1,5 +1,5 @@
 window.SKBC_KIDS_CONFIG = {
   // Pega aqui la URL del Web App de Apps Script cuando lo publiques.
   // Debe terminar en /exec.
-  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbyP4-bsqB235RNqJYCMqn3W-OQrBQFCAaBthin6sL38I9BPV8A4xHpnPJaL1f6tHyLLCA/exec'
+  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxzCS6inq2VjRAaZwmJ5m4RRk-MiM0xdAwyVPMxO5H75Squ-4rYIriiQKYWwPvnmGFtdQ/exec'
 };
